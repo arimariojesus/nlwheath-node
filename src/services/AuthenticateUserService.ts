4ac9ffaedf4119f43c852bd15a01ef7420d02e78
Arimario Jesus <arimario.jesus@hotmail.com>
2021-10-21 00:26:15 -0300

    feat: get access token and add sqlite integration and user model

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,19 +1,32 @@
 import axios from 'axios';
+import prismaClient from '../prisma';
+
 /**
  * Receber code(string)
  * Recuperar o access_token no github
+ * Recuperar infos do user no github
  * Verificar se o usuario existe no DB
  * ---- SIM = Gera um token
  * ---- NAO = Cria no DB, gera um token
  * Retornar o token com as infos do user
  */
 
+interface IAuthenticateTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
 
 class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https/github.com/login/oauth/access_token';
 
-    const { data: response } = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAuthenticateTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -24,7 +37,32 @@ class AuthenticateUserService {
       },
     });
 
-    return response;
+    const response = await axios.get<IUserResponse>('https://api.github.com/user', {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
+      },
+    });
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    return response.data;
   }
 }
 
